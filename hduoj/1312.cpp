b4cff20d386b2429a5376cd7a2040b93f595cb26
guodongxiaren <879231132@qq.com>
2014-07-04 09:00:08 +0800

    up

---
+++ hduoj/1312.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int num;
+int w,h;
+bool g[20][20];
+void bfs(int i,int j)
+{
+    if(i-1>=0&&g[i-1][j])
+    {
+        num++;
+        g[i-1][j]=false;
+        bfs(i-1,j);
+    }
+    if(j-1>=0&&g[i][j-1])
+    {
+        num++;
+        g[i][j-1]=false;
+        bfs(i,j-1);
+    }
+    if(i+1<h&&g[i+1][j])
+    {
+        num++;
+        g[i+1][j]=false;
+        bfs(i+1,j);
+    }
+    if(j+1<w&&g[i][j+1])
+    {
+        num++;
+        g[i][j+1]=false;
+        bfs(i,j+1);
+    }
+}
+int main()
+{
+    char c;
+    int si,sj;
+    while(cin>>w>>h)
+    {
+        if(!w&&!h)
+            break;
+        num=1;
+        for(int i=0;i<h;i++)
+        for(int j=0;j<w;j++)
+        {
+            cin>>c;
+            if(c=='.')
+                g[i][j]=true;
+            else if(c=='@')
+            {
+                si=i;
+                sj=j;
+            }
+        }
+        bfs(si,sj);
+        cout<<num<<endl;
+        memset(g,false,sizeof(g));
+    }
+}
