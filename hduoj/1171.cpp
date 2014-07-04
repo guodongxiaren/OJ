8eb8b9f822cec1cb0a46610d26a81964dfbd198c
guodongxiaren <879231132@qq.com>
2014-07-04 08:17:27 +0800

    up1171

---
+++ hduoj/1171.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+bool g[26][26];
+bool used[26];
+bool bfs(int i)
+{
+    used[1]=true;
+    for(int j=0;j<26;j++)
+    {
+        if(i!=j&&!used[j]&&g[i][j])
+        {
+            if(i!=1)
+                g[1][j]=true;
+            used[j]=true;
+            bfs(j);
+        }
+    }
+    return g[1][12];
+}
+int main()
+{
+    char s[100];
+    while(cin>>s)
+    {
+        if(s[0]!='0')
+        {
+            int len=strlen(s);
+            g[s[0]-'a'][s[len-1]-'a']=true;
+            continue;
+        }
+        if(bfs(1))
+            cout<<"Yes."<<endl;
+        else
+            cout<<"No."<<endl;
+        memset(g,false,sizeof(g));
+        memset(used,false,sizeof(used));
+    }
+}
+
