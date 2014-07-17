8da738d9c1843ff99a609cfafa9b7cf0a455f462
guodongxiaren <879231132@qq.com>
2014-07-17 19:49:56 +0800

    4324

--- poj/2225.cpp
+++ poj/2225.cpp
@@ -4,42 +4,43 @@ using namespace std;
 #include<cstdio>
 #include<cstring>
 #define INF 0x3f3f3f3f
-#define M 1000
 struct node
 {
-    int x,y,z,level;
+    int x,y,z;
+    int level;
+    node(int i,int j,int k,int l):x(i),y(j),z(k),level(l){};
+    void set(int i,int j,int k,int l)
+    {
+        x=i;
+        y=j;
+        z=k;
+        level=l;
+    }
 };
 int d[6][3]={0,1,0,0,-1,0,1,0,0,-1,0,0,0,0,1,0,0,-1};
 char m[10][10][10];
-int used[10][10][10];
+bool used[10][10][10];
 int s[3],e[3];
 int n,step;
-node q[M];
+queue<node> q;
 void bfs(int x,int y,int z)
 {
     int i,j,k,l;
-    int front=0,rear=0;
-    node pos;
-    pos.x=x;
-    pos.y=y;
-    pos.z=z;
-    pos.level=0;
-    q[front]=pos;
-    front=(front+1)%M;
-    while(rear!=front)
+    node pos(x,y,z,0);
+    q.push(pos);
+    used[x][y][z]=1;
+    while(!q.empty())
     {
-        pos = q[rear];
+        pos = q.front();
         i = pos.x;
         j = pos.y;
         k = pos.z;
         l = pos.level;
-        rear=(rear+1)%M;
-        if(l<used[i][j][k])
-            used[i][j][k]=l;
+        q.pop();
         if(i==e[2]&&j==e[1]&&k==e[0])
         {
-            if(used[i][j][k]<step)
-                step=used[i][j][k];
+            if(l<step)
+                step=l;
             continue;
         }
         for(int t=0;t<6;t++)
@@ -47,14 +48,11 @@ void bfs(int x,int y,int z)
             x=i+d[t][0];
             y=j+d[t][1];
             z=k+d[t][2];
-            if(x<0||x>=n||y<0||y>=n||z<0||z>=n||m[x][y][z]!='O'||l+1>=used[x][y][z])
+            if(x<0||x>=n||y<0||y>=n||z<0||z>=n||m[x][y][z]!='O'||used[x][y][z])
                 continue;
-            pos.x=x;
-            pos.y=y;
-            pos.z=z;
-            pos.level=l+1;
-            q[front]=pos;
-            front=(front+1)%M;
+            pos.set(x,y,z,l+1);
+            used[x][y][z]=1;
+            q.push(pos);
         }
     }
 }
@@ -63,7 +61,7 @@ int main()
     char str[6];
     while(cin>>str>>n)
     {
-        memset(used,0x3f,sizeof used);
+        memset(used,0,sizeof used);
         step = INF;
         for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)
