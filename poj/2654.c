a23804a4679a5e77772c775c914c7af33fc838c2
guodongxiaren <879231132@qq.com>
2014-09-05 20:42:44 +0800

    2654

--- poj/2654.c
+++ poj/2654.c
@@ -1,30 +1,34 @@
 #include<stdio.h>
 #include<string.h>
+int w[200],l[200];
 int p1,p2,i,j,k,m,n;
 char m1[10],m2[10];
 int main()
 {
-    for(m=0; 1<=scanf("%d%d",&n,&k)&&n; m++)
+    int flag=0;
+    while(1)
     {
-        if(m)
-        {
+        scanf("%d",&n);
+        if(!n)
+            break;
+        scanf("%d",&k);
+        if(flag)
             printf("\n");
-            memset(w,0,sizeof w);
-            memset(l,0,sizeof l);
-        }
+        else
+            flag=1;
+        memset(w,0,sizeof w);
+        memset(l,0,sizeof l);
         for(i=0; i<k*n*(n-1)/2; i++)
         {
             scanf("%d%s%d%s",&p1,m1,&p2,m2);
-            if(!strcmp(m1,"rock")&&!strcmp(m2,"scissors")||
-                    !strcmp(m1,"scissors")&&!strcmp(m2,"paper"))||
-                !strcmp(m1,"paper")&&!strcmp(m2,"rock"))
+            if((m1[0]=='r'&&m2[0]=='s')||(m1[0]=='s'&&m2[0]=='p')||
+                    (m1[0]=='p'&&m2[0]=='r'))
             {
                 w[p1]++;
                 l[p2]++;
             }
-            if(!strcmp(m2,"rock")&&!strcmp(m1,"scissors")||
-                        !strcmp(m2"scissors")&&!strcmp(m1,"paper"))||
-                !strcmp(m2,"paper")&&!strcmp(m1,"rock"))
+            if((m2[0]=='r'&&m1[0]=='s')||(m2[0]=='s'&&m1[0]=='p')||
+                    (m2[0]=='p'&&m1[0]=='r'))
             {
                 w[p2]++;
                 l[p1]++;
@@ -41,5 +45,3 @@ int main()
     if(n)
         printf("extraneous input!%d\n",n);
 }
-}
-
